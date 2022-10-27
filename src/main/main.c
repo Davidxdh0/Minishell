4b8fdc19d4e855d30d58a880259d96c9e9925ea2
Bram Provoost <bprovoos@f1r2s16.codam.nl>
2022-10-27 19:10:37 +0200

    update

--- src/main/main.c
+++ src/main/main.c
@@ -6,54 +6,12 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/06 14:36:20 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/10/26 20:20:47 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/10/27 18:28:40 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-void	make_sure_line_is_empty(char **line)
-{
-	if (*line)
-	{
-		free (*line);
-		*line = (char *) NULL;
-	}
-}
-
-/*
-	Set the display name in the termian and reading the input from the termial
-*/
-void	line_reader(char **line, const char *display_name)
-{
-	make_sure_line_is_empty(line);
-	*line = readline(display_name);
-	add_line_in_history(line);
-}
-
-/* Program flow
-1. lexer
-1.1. check syntax
-1.2. puts the characters together into words called tokens
-2. parser
-2.1. read the tokens
-2.2. build the command table
--------(index, command, flags, envp)
--------(in, out, err)
-3. expander
-4. executor
-4.1. read command table
-4.x. creating pipes
-4.x. creating processes
-
-To do:
-[X] Header dependency in makefile
-[X] Creata a flag option for giving command through argv
-[ ] Make unit testers
-
-Optional:
-[ ] *.d files in seperate folder
-*/
 int	shell(char *line, char **envp)
 {
 	char		**tokens;
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 17:46:28 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/10/27 14:44:29 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/10/27 17:06:28 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,37 +31,13 @@ typedef struct s_line_lst
 }	t_line_lst;
 
 typedef enum{
-	empty = -1,
-	cmd,
-	file,
-	pipe,
-	text,
-	var,
-	redirect
+	e_cmd,
+	e_file,
+	e_pipe,
+	e_text,
+	e_var,
+	e_redirect
 }	note_type;
-
-/* Add a node at the end */
-void	add_node_end(t_line_lst **node);
-
-/* Add a node before a given node */
-void	insert_node_before(t_line_lst *node);
-
-void	insert_node_after(t_line_lst *node);
-
-/* Loop to the first node */
-t_line_lst	*get_first_node(t_line_lst *node);
-
-/* Add a node at the front */
-void	add_node_front(t_line_lst *node);
-
-/* Remove given node */
-void	remode_node(t_line_lst *node);
-
-/* Remove the list */
-void	remove_list(t_line_lst *node);
-
-/* Count length of list */
-int		len_of_list(t_line_lst *node);
 
 /* The parser processes the tokens build the list with */
 t_line_lst	*parser(char **tokens);
