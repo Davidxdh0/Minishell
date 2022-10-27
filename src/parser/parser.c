63c74eb913fea16d0cc1f49feca7074a987265c3
Bram Provoost <bprovoos@f1r2s16.codam.nl>
2022-10-27 08:46:59 +0200

    add prototypes of double linked list

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/13 15:28:56 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/10/26 20:20:28 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/10/27 08:40:57 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,57 @@ int	amount_of_tokens(char **tokens)
 	return (list_len);
 }
 
-/*
-	The parser processes the tokens according to a grammar and build the command 
-	table.
-*/
-t_line_lst	*parser(char	**tokens)
+/* Add a node after a given node */
+t_line_lst	*insert_node_after(t_line_lst *node)
 {
-	int			list_len;
+	
+}
+
+/* Add a node at the end */
+t_line_lst	*add_node_end(t_line_lst *node)
+{
+	
+}
+
+/* Add a node before a given node */
+t_line_lst	*insert_node_before(t_line_lst *node)
+{
+	
+}
+
+/* Add a node at the front */
+t_line_lst	*add_node_front(t_line_lst *node)
+{
+	
+}
+
+/* Remove given node */
+t_line_lst	*remode_node(t_line_lst *node)
+{
+	
+}
+
+/* Remove the list */
+t_line_lst	*remove_list(t_line_lst *node)
+{
+	
+}
+
+/* Count length of list */
+int			len_of_list(t_line_lst *node)
+{
+	
+}
+
+/*
+	The parser processes the tokens build the list with .
+*/
+t_line_lst	*parser(char **tokens)
+{
+	int			len_list;
 	t_line_lst	*line_lst;
 
-	list_len = amount_of_tokens(tokens);
+	len_list = amount_of_tokens(tokens);
 	line_lst = NULL;
 	line_lst = (t_line_lst *)malloc(sizeof(t_line_lst));
 	// line_lst_>value = "";
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 17:46:28 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/10/26 20:20:24 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/10/27 08:44:10 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,28 @@ typedef struct s_line_lst
 	struct s_line_lst	*prev;
 }	t_line_lst;
 
-t_line_lst	*parser(char	**tokens);
+/* Add a node after a given node */
+t_line_lst	*insert_node_after(t_line_lst *node);
+
+/* Add a node at the end */
+t_line_lst	*add_node_end(t_line_lst *node);
+
+/* Add a node before a given node */
+t_line_lst	*insert_node_before(t_line_lst *node);
+
+/* Add a node at the front */
+t_line_lst	*add_node_front(t_line_lst *node);
+
+/* Remove given node */
+t_line_lst	*remode_node(t_line_lst *node);
+
+/* Remove the list */
+t_line_lst	*remove_list(t_line_lst *node);
+
+/* Count length of list */
+int			len_of_list(t_line_lst *node);
+
+/* The parser processes the tokens build the list with */
+t_line_lst	*parser(char **tokens);
 
 #endif
