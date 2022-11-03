4864c6637ef2fe3011d2abbf51f4efb4a9cb203e
Bram Provoost <bprovoos@f1r2s16.codam.nl>
2022-11-03 20:40:37 +0100

    update

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 17:46:28 by bprovoos      #+#    #+#                 */
-/*   Updated: 2022/11/03 18:47:46 by bprovoos      ########   odam.nl         */
+/*   Updated: 2022/11/03 20:36:12 by bprovoos      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ t_line_lst	*parser(char *line);
 void		delete_t_list(t_line_lst **head);
 void		add_at_end_of_list(t_line_lst **head, int type, char *value);
 void		show_t_list(t_line_lst *node, char *line);
+char		*type_to_string(note_type type);
 int			length_of_list(t_line_lst *node);
 
 #endif
