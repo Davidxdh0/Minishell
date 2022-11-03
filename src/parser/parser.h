01c9c85ec798bd48a81cc5c039ad900445764207
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2022-11-03 18:23:59 +0100

    test3

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,11 +6,7 @@
 /*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/26 17:46:28 by bprovoos      #+#    #+#                 */
-<<<<<<< HEAD
-/*   Updated: 2022/11/03 17:46:15 by dyeboa        ########   odam.nl         */
-=======
-/*   Updated: 2022/11/03 14:05:44 by bprovoos      ########   odam.nl         */
->>>>>>> fc121cfdfff8a2bd03b05df1e44f5e67393c9115
+/*   Updated: 2022/11/03 18:23:46 by dyeboa        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,13 +48,8 @@ typedef enum{
 t_line_lst	*parser(char *line);
 
 void		delete_t_list(t_line_lst **head);
-<<<<<<< HEAD
 void		add_at_end_of_list(t_line_lst **head, int type, char *value);
 void		show_t_list(t_line_lst *node);
 int			length_of_list(t_line_lst *node);
-=======
-
-void		show_t_list(t_line_lst *node);
->>>>>>> fc121cfdfff8a2bd03b05df1e44f5e67393c9115
 
 #endif
