78453596c414c8d73128c23acac8fe0b21594e90
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2023-06-20 15:02:42 +0200

    signal

--- src/parser/parser_util3.c
+++ src/parser/parser_util3.c
@@ -6,7 +6,7 @@
 /*   By: dyeboa <dyeboa@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/15 21:28:38 by dyeboa        #+#    #+#                 */
-/*   Updated: 2023/06/20 14:55:05 by dyeboa        ########   odam.nl         */
+/*   Updated: 2023/06/20 15:02:21 by dyeboa        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ int	count_redirectss(t_line_lst *head)
 
 void	signal_bs1(int sig)
 {
-	return ;
 	sig++;
 }
 
