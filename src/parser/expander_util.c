c2b3061f288a5b765b23f4d10dd9bdbbce192ed9
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2023-06-22 20:12:04 +0200

    hd

--- src/parser/expander_util.c
+++ src/parser/expander_util.c
@@ -6,7 +6,7 @@
 /*   By: dyeboa <dyeboa@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/01 20:04:47 by dyeboa        #+#    #+#                 */
-/*   Updated: 2023/06/20 20:26:01 by dyeboa        ########   odam.nl         */
+/*   Updated: 2023/06/22 20:10:35 by dyeboa        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,7 @@ void	expand_heredoc_word(char *value, t_envp *new_envp, int fd)
 		{
 			begin = i;
 			while (value[i] != '\0' && ft_isascii(value[i]) && \
-			value[i] != '$' && value[i] != ' ')
+			value[i] != '$' && value[i] != ' ' && value[i] != '\"')
 				i++;
 			ft_putstr_fd(change_heredoc_str(value, begin, i, new_envp), fd);
 		}
